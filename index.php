c2f3faeaa72e6d946968ee98a74dd8f70945febf
melanchthon <mykoladjeria@gmail.com>
2013-10-15 14:45:08 +0400

    fix syntax

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
-//ini_set('display_errors', 1);
 error_reporting(E_ALL);
-define('BASE_PATH',$_SERVER['DOCUMENT_ROOT']);
-
 require_once('Application/bootstrap.php');
