eaf09415796669e84abb3226146d174e723d3ddb
melanchthon <mykoladjeria@gmail.com>
2013-10-15 14:31:17 +0400

    fix syntax

--- Application/Core/Post.php
+++ Application/Core/Post.php
@@ -4,13 +4,4 @@ class Core_Post
 	public $title;
 	public $content;
 	public $createTime;
-
-	/*public function __construct ($title,$text,$create_time)
-	{
-		$this->title = $title;
-		$this->text = $text;
-		$this->create_time = $create_time;
-
-	}*/
-
 }
