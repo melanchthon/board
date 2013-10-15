cdfc3b3ba98310b9acb12ac0bf710ffa9c6c2ec4
melanchthon <mykoladjeria@gmail.com>
2013-10-15 14:16:12 +0400

    add posibility to change db settings from config file

--- Application/Core/DbConnection.php
+++ Application/Core/DbConnection.php
@@ -10,6 +10,7 @@ class Core_DbConnection
 	static public function getInstance()
 	{
 		$config = Config::getDbConfig();
+		
 		if (!self::$instance) {
 			self::$instance = new PDO("mysql:host={$config['host']};dbname={$config['name']}",$config['user'],$config['pass']);
 			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
