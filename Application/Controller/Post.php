cfa9a05aaa473f7d29d9b98443715d40d3f2726e
melanchthon <mykoladjeria@gmail.com>
2013-10-19 01:21:08 +0400

    add validation

--- Application/Controller/Post.php
+++ Application/Controller/Post.php
@@ -2,6 +2,8 @@
 class Controller_Post extends Core_Controller
 {
 	
+	private $error = array();
+	
 	public function actionCreate ()
 	{
 		$post = new Core_Post();
@@ -9,17 +11,30 @@ class Controller_Post extends Core_Controller
 		$this->model = new Model_Post();
 		
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			if (!empty($_POST['title'])){
-				$post->title = trim($_POST['title']);
-			}
-			if (!empty($_POST['content'])){
-				$post->content = trim($_POST['content']);
-			}
+			
+			$post->title = trim($_POST['title']);
+			$post->content = trim($_POST['content']);
 			$post->createTime=time();
-
+			
+			$this->validate($post);
+			if (empty($this->error)){
 			$this->model->createPost($post);
 			header('Location: ../');
+			} else {
+				$this->view->render('View_PostCreate.php','View_Template.php',array('error'=>$this->error));
+			}
 		}
-		$this->view->render('View_Main.php', 'View_PostCreate.php');
+		$this->view->render('View_PostCreate.php','View_Template.php');
+	}
+	
+	private function validate (Core_Post $post)
+	{
+		if (empty($post->title)){
+			$this->error[] = "Заголовок не должен быть пустым";
+		}
+		
+		if (empty($post->content)){
+			$this->error[] = "Пост не должен быть пустым";
+		}
 	}
 }
