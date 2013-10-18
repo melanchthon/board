2abc33d62ef57f4f072656d75c68eaa48bde4d2d
melanchthon <mykoladjeria@gmail.com>
2013-10-19 01:42:50 +0400

    add profiler

--- Application/bootstrap.php
+++ Application/bootstrap.php
@@ -2,7 +2,11 @@
 require_once('Core/Autoload.php');
 spl_autoload_register(array('Core_Autoload', 'loadClass'));
 
-
-
+Core_Profiler::setScriptStart();//время начала выполнения скрипта
 $frontController = new Core_FrontController();
-$frontController->run();
+$frontController->run();//время конца выполнения скрипта
+Core_Profiler::setScriptEnd();
+if (Config::isProfilerEnabled()){
+	echo '<hr/>';
+	Core_Profiler::printTable();
+}
