496141b420a923e7b4c1b587e69f347c065c7e2f
melanchthon <mykoladjeria@gmail.com>
2013-10-23 23:54:28 +0400

    add get parameters in render method

--- Application/Core/View.php
+++ Application/Core/View.php
@@ -1,11 +1,16 @@
 <?php
 class Core_View
 {
-    public function render($content_view, $template_view, $data = null)
+    public function render($content_view, $template_view, $data = null, $params = array())
     {
         if(is_array($data)) {  
             extract($data);
         }
+
+		foreach($params as $key=>$value){
+			$_GET[$key] = $value;
+		}
+		
         include ('Application/View/'.$template_view);
     }
 	
