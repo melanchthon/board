8cdab4351dde3d07170fa06623251595f540340f
melanchthon <mykoladjeria@gmail.com>
2013-10-26 02:00:56 +0400

    change profiler connection

--- Application/bootstrap.php
+++ Application/bootstrap.php
@@ -3,10 +3,11 @@ $start = microtime(true);//время начала роботы скрипта
 require_once('Core/Autoload.php');
 spl_autoload_register(array('Core_Autoload', 'loadClass'));
 
-
 $profiler = new Core_Profiler();
 $pdo = Core_DbConnection::getInstance();
-$pdo->setProfiler($profiler);
+if (Config::isProfilerEnabled()){
+	$pdo->setProfiler($profiler);
+}
 $frontController = new Core_FrontController();
 $frontController->run();
 $end = microtime(true);//время конца роботы скрипта
