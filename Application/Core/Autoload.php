0d13d284b4718b0545caa2dea18e257b14bca932
melanchthon <mykoladjeria@gmail.com>
2013-10-26 01:36:17 +0400

    add 404-error page

--- Application/Core/Autoload.php
+++ Application/Core/Autoload.php
@@ -7,6 +7,8 @@ class Core_Autoload
 		$path = "Application/".implode('/',$path).'.php';
 		if(file_exists($path)) {
 			require_once($path);	
+		} else {
+			require_once("Application/View/404.php");
 		}
 	}
 }
