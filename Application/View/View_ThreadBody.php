8bec56f7abc1b03cd2a9990334d4d6dfd5609038
melanchthon <mykoladjeria@gmail.com>
2013-10-26 02:01:52 +0400

    change user name displaing

--- Application/View/View_ThreadBody.php
+++ Application/View/View_ThreadBody.php
@@ -1,4 +1,5 @@
-<div class="b-post first-post">            
+<div class="b-post first-post">     
+       
                 <div class="post-number"><?=$this->h($post->id);?></div>
                 
                 <p><?=$this->h($post->content); ?></p>
@@ -6,7 +7,16 @@
 			
 			<?php foreach ($comments as $comment):?>
             <div class="b-post">            
-                <div class="post-number"><?=$this->h($comment->id); ?> | <?=$this->h($comment->name);?></div>
+                <div class="post-number">
+					<?=$this->h($comment->id); ?> | 
+					<?php
+						if (!empty($comment->name)){
+							echo $this->h($comment->name);
+						} else {
+						echo Config::getDefaultName();
+						}
+					?>
+				</div>
                 
                 <p><?=$this->h($comment->content); ?></p>        
             </div>
