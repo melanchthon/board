4095be5aadf4b4440080c3d30c2aed1702dcffc4
melanchthon <mykoladjeria@gmail.com>
2013-11-03 02:42:34 +0300

    dispaly only 10 links in pagination view

--- Application/Core/Pagination.php
+++ Application/Core/Pagination.php
@@ -15,15 +15,37 @@ class Core_Pagination
 	public function generate()
 	{
 		if($this->pagesCount <= 1){
-			exit;
+			return false;
 		}
 		
 		$this->view->renderPartial('View_Pagination.php',array(
 		'pagesCount'=>$this->pagesCount,
 		'currentPage'=>$this->currentPage,
+		'firstVisiblePage'=>$this->getFirstVisiblePage(),
+		'lastVisiblePage'=>$this->getLastVisiblePage(),
 		)
 		);
 	}
-	
 
+
+	public function getLastVisiblePage()
+	{
+		$pages = min($this->pagesCount,9);
+		if($this->currentPage>=9 && $this->pagesCount>9 ){
+			return $this->currentPage+1;
+		} else {
+			return $pages;
+		}
+	}
+	
+	public function getFirstVisiblePage()
+	{
+		
+		if($this->currentPage<8 ){
+			return 0;
+		} else {
+			return $this->currentPage-8;
+		}
+	}
+	
 }
