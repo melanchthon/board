4d94364b4848fc4e5727c05b3b0067f2c1529882
melanchthon <mykoladjeria@gmail.com>
2013-11-16 03:21:18 +0300

    page, displayed after succes registration

---
+++ Application/View/View_registrationSucces.php
@@ -0,0 +1,21 @@
+<div class="l-header">
+    <div class="wrap">
+        <h1>Программирование // доска обсуждений</h1>
+    </div>
+    <div class="overlay"></div>
+</div>
+
+<div class="page-threads-list">
+ 
+	<div class="b-threads-list">
+		<div class="b-thread">
+			<div class="b-post first-post">
+				
+				<h2 class="thread-topic">Регистрация прошла успешно, можете <a href="<?=Config::getBasePath()?>/user/login">зайти на сайт</a> под своим именем.</h2>
+				
+			</div> 
+		</div>
+		
+	</div>
+	
+</div> 
