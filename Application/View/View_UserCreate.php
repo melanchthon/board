4894411b8ef024a6811696ce81d59455c143c684
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:46:21 +0300

    add csrf, captcha and honeyopt fields

--- Application/View/View_UserCreate.php
+++ Application/View/View_UserCreate.php
@@ -15,19 +15,41 @@
         <div class="row">
             <div class="row-comment">Ваш логин должен состоять не менее чем из 5 символов.</div>
             <label class="row-label" for="add-text">Ваш логин:</label>
-            <input type ="text" name="name" class="input-wide"/>
+            <input type ="text" name="name" class="input-wide" value="<?=(isset($name) ? $name : NULL);?>"/>
         </div>
 		
 		<div class="row">            
 			<div class="row-comment">Ваш пароль должен состоять не менее чем из 8 символов и содержать не только цифры, но ибуквы.</div>
             <label class="row-label" for="add-name">Ваш пароль:</label>
-            <input type="text" name="pass" class="input-wide">
+            <input type="text" name="pass" class="input-wide" value="<?=(isset($pass) ? $pass : NULL);?>">
 		</div>
-        
-
-       <div class="row row-buttons">
+		
+		<div class='honeypot'>
+			<label class='row-label'>Оставте это поле пустым:</label>
+			<input type='text' name='honeypot'>
+		</div>
+		
+		<input type='hidden' name='csrf' value ='<?=$csrf->getToken();?>'/>
+		
+		<div class="row">            
+            <label class="row-label" for="add-name">Запомнить меня:</label>
+            <input type="checkbox" checked name="remember">
+		</div>
+		<input type='hidden' name='csrf' value ='<?=$csrf->getToken();?>'/>
+	
+		<div class='captcha'>
+			<?php 
+				$captcha = new Core_Captcha();
+				if($captcha->isCaptchaRequired()){
+					$captcha->setCaptcha();
+					$captcha->view();
+				}
+			?>
+		</div>
+		
+		<div class="row row-buttons">
             <button class="button-action button-main">Зарегистрироваться</button>
-       </div>
+        </div>
    </form>
    <a href="<?=Config::getBasePath()?>" class="button-left back-link">← вернуться на главную</a>
 </div>
