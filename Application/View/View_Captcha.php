7237efa3ecbc282a122edc1729a88130ad42a422
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:34:02 +0300

    display captcha form

---
+++ Application/View/View_Captcha.php
@@ -0,0 +1,6 @@
+<!--captcha form-->
+<div class='captcha'>
+			<img src='<?=Config::getBasePath()?>/captcha/create' />
+			<label class="row-label" for="add-text">Введите капчу:</label>
+			<input type='text' name ='captcha'>
+</div>
