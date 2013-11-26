b1a67bf01238809b390c09241a9dd333b4b16c5c
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:53:40 +0300

    add 'basepath' constanst

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 header("Content-Type: text/html;charset=utf-8");
+mb_internal_encoding("UTF-8");
 error_reporting(-1);
+define('BASEPATH',__DIR__);
 
 
 require_once('Application/bootstrap.php');
