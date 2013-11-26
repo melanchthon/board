106d63e12933835ba0fb03fcc024595316cd3791
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:09:47 +0300

    add captcha requirements, if there was more than 3 failed login attempts

--- Application/Controller/User.php
+++ Application/Controller/User.php
@@ -7,18 +7,21 @@ class Controller_User extends Core_Controller
 	{
 		$user = new Core_User();
 		$this->model = new Model_User();
-		$this->view = new Core_View();
+		$this->view = new Core_View();	
+		$this->isCaptchaRequired();
 		
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$user->name = $_POST["name"];
-			$user->pass = $_POST["pass"];
-		
+			$this->getPost($user);
 			$error = $this->model->validate($user);
 			if (empty($error)){
-				$this->model->createUser($user);
-				header("Location:".Config::getbasePath()."/user/succes");
-			} else {
-				$this->view->render('View_UserCreate.php','View_Template.php', array('error'=>$error));
+				$userData = $this->model->createUser($user);
+				$authManager = new Core_Auth();
+				$authManager->login($userData,$user->remember);
+				header("Location:".Config::getbasePath().'/user/succes');
+			} else {	
+				$this->view->render('View_UserCreate.php','View_Template.php', array('error'=>$error,
+																					'name'=>$user->name,
+																					'pass'=>$user->pass));																
 			}
 			
 		}
@@ -27,35 +30,39 @@ class Controller_User extends Core_Controller
 	}
 	
 	public function actionLogin()
-	{
+	{	
 		$user = new Core_User();
 		$this->model = new Model_User();
 		$this->view = new Core_View();
+		$this->isCaptchaRequired();
 		
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-			$user->name = $_POST["name"];
-			$user->pass = $_POST["pass"];
-			$remember = ($_POST["remember"] == "yes") ? true : false;
-			
-			$userData = $this->model->authenticate($user);
+			$this->getPost($user);
+			$userData = $this->model->authenticate($user);//get $user oject if authentication is succes
 			if ($userData){
 				$authManager = new Core_Auth();
-				$authManager->login($userData,$remember);
+				$authManager->login($userData,$user->remember);
 				header("Location:".Config::getbasePath());
 			} else {
+				//if login attempt is unsuccesfull, display login form again with error messages
 				$error = $this->model->getErrors();
-				$this->view->render('View_UserLogin.php','View_Template.php', array('error'=>$error));
-			}
-			
+				$this->setFailsCount(); //increase counter of failed  login attempts
+				$this->view->render('View_UserLogin.php','View_Template.php', array('error'=>$error,
+																					'name'=>$user->name,
+																					'pass'=>$user->pass));
+			}	
 		}
 		$this->view->render('View_UserLogin.php','View_Template.php');
 	}
 	
 	public function actionLogout()
 	{
-		$authManager = new Core_Auth();
-		$authManager->logout();
-		header("Location:".Config::getbasePath());	
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$authManager = new Core_Auth();
+			$authManager->logout();
+			header("Location:".Config::getbasePath());	
+			}
+		return false;
 	}
 	
 	public function actionSucces()
@@ -63,5 +70,38 @@ class Controller_User extends Core_Controller
 		$this->view = new Core_View();
 		$this->view->render('View_registrationSucces.php','View_Template.php');
 	}
+
+	
+	public function getFailsCount()
+	{
+		//return number of failed login attempts from this ip;
+		$ip = $_SERVER['REMOTE_ADDR'];
+		if(isset($_SESSION[$ip])){
+			return $_SESSION[$ip]['failsCounter'];
+		}
+	}
+	
+	public function setFailsCount()
+	{
+		//increase number of failed attempts by 1 and store it in the session
+		$ip = $_SERVER['REMOTE_ADDR'];
+		if(isset($_SESSION[$ip])){
+			$_SESSION[$ip]['failsCounter'] += 1;
+		} else {
+			$_SESSION[$ip]['failsCounter'] = 1;
+		}
+	}
+	
+	private function isCaptchaRequired()
+	{
+		//if  amount of failed login attempts is more then 3, captcha is required
+		$auth = new Core_Auth();
+		$capthca = new Core_Captcha();
+		if($this->getFailsCount() >= 3){
+			$capthca->requireCaptcha();
+		} else {
+			$capthca->disableCaptcha();
+		}
+	}	
 	
 }
