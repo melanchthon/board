1de8b220fa0c4b319bcaf418ae63f908cb56a0b9
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:00:38 +0300

    add captcha

--- Application/Controller/Post.php
+++ Application/Controller/Post.php
@@ -2,30 +2,40 @@
 class Controller_Post extends Core_Controller
 {
 	
-	private $error;
-	
 	public function actionCreate ()
 	{
+		
 		$post = new Core_Post();
 		$this->view = new Core_View();
 		$this->model = new Model_Post();
+		$this->isCaptchaRequired();
+		
 		
 		if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			
-			$post->title = trim($_POST['title']);
-			$post->content = trim($_POST['content']);
+			$this->getPost($post);
 			$post->createTime=$post->bumped=date('Y-m-d H:i:s');
-			$post->name =trim($_POST['name']);
 			$this->error = $this->model->validate($post);
+			
 			if (empty($this->error)){
 				$this->model->createPost($post);
 				header('Location: ../');
 			} else {
-				$this->view->render('View_PostCreate.php','View_Template.php',array('error'=>$this->error));
+				$this->view->render('View_PostCreate.php','View_Template.php',array('error'=>$this->error, 'title'=>$post->title, 'content'=>$post->content));
 			}
 		}
 		$this->view->render('View_PostCreate.php','View_Template.php');
 	}
 	
+	private function isCaptchaRequired()
+	{
+		$auth = new Core_Auth();
+		$capthca = new Core_Captcha();
+		if(!$auth->isLogged()){
+			$capthca->requireCaptcha();
+		} else {
+			$capthca->disableCaptcha();
+		}
+	}	
 	
 }
