d5c3b71ac0c74e689104add965972d4353235c2d
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:47:53 +0300

    add csrf, captcha and honeypot fields

--- Application/View/View_UserLogin.php
+++ Application/View/View_UserLogin.php
@@ -15,18 +15,36 @@
         <div class="row">
             
             <label class="row-label" for="add-text">Ваш логин:</label>
-            <input type ="text" name="name" class="input-wide"/>
+            <input type ="text" name="name" class="input-wide" value="<?=(isset($name) ? $name : NULL);?>" />
         </div>
 		
 		<div class="row">            
             <label class="row-label" for="add-name">Ваш пароль:</label>
-            <input type="text" name="pass" class="input-wide">
+            <input type="text" name="pass" class="input-wide" value="<?=(isset($pass) ? $pass : NULL);?>">
 		</div>
         
+		<div class='honeypot'>
+			<label class='row-label'>Оставте это поле пустым:</label>
+			<input type='text' name='honeypot'>
+		</div>
+		
 		<div class="row">            
             <label class="row-label" for="add-name">Запомнить меня:</label>
-            <input type="checkbox" checked name="remember" value="yes">
+            <input type="checkbox" checked name="remember">
 		</div>
+		
+		<div class='captcha'>
+			<?php 
+				//if captcha is required, display captcha form
+				$captcha = new Core_Captcha();
+				if($captcha->isCaptchaRequired()){
+					$captcha->setCaptcha();
+					$captcha->view();
+				}
+			?>
+		</div>
+		
+		
         
 
        <div class="row row-buttons">
