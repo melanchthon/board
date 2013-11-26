f5d2f9e37e4e396fee78e363d5729b9206af79a7
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:44:50 +0300

    change css styles for logged and aonnymous users

--- Application/View/View_ThreadBody.php
+++ Application/View/View_ThreadBody.php
@@ -1,21 +1,23 @@
-<div class="b-post first-post">     
+			<div class="b-post first-post">     
        
-                <div class="post-number"><?=$this->h($post->id);?></div>
-                
+                <div class="post-number">
+					#<?=$this->h($post->id);?>
+				</div>
+                <div class="post-name">
+					<?=(!empty($post->name) ? $this->h($post->name) : Config::getDefaultName());?> 
+				</div>
                 <p><?=$this->h($post->content); ?></p>
             </div>
 			
 			<?php foreach ($comments as $comment):?>
+			
             <div class="b-post">            
-                <div class="post-number">
-					<?=$this->h($comment->id); ?> | 
-					<?php
-						if (!empty($comment->name)){
-							echo $this->h($comment->name);
-						} else {
-						echo Config::getDefaultName();
-						}
-					?>
+               <div class="post-name <?=!empty($comment->name) ? 'authorized' : '';?>">
+					<?=(!empty($comment->name) ? $this->h($comment->name) : Config::getDefaultName());?> 
+				</div>
+			   
+			   <div class="post-number">
+					#<?=$this->h($comment->id); ?> | <?=$this->h($comment->create_time); ?> 
 				</div>
                 
                 <p><?=$this->h($comment->content); ?></p>        
