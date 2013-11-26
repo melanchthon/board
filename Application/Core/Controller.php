4b65ad6b5db123387d3d01546cea1169ef7279a2
melanchthon <mykoladjeria@gmail.com>
2013-11-26 21:26:36 +0300

    add getPost() method, which get values from $_POST array, and save them as properties of the argument object

--- Application/Core/Controller.php
+++ Application/Core/Controller.php
@@ -3,4 +3,14 @@ class Core_Controller
 {
 	public $view;
 	public $model;
+	public $error;
+	
+	public function getPost($object)
+	{
+		//gets data from $_POST array and makes them properties of argument class
+		foreach($_POST as $key=>$value){
+			$object->$key = isset($key) ? trim($value) : null;
+		}
+		return $object;
+	}
 }
